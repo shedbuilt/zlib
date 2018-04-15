c48573d6cdaff43b816a230632611ef9dddbb541
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:30:01 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ./configure --prefix=/usr
-make -j $SHED_NUMJOBS
-make "DESTDIR=${SHED_FAKEROOT}" install
-mkdir -pv ${SHED_FAKEROOT}/lib
-mv -v ${SHED_FAKEROOT}/usr/lib/libz.so.* ${SHED_FAKEROOT}/lib
-ln -sfv ../../lib/$(readlink ${SHED_FAKEROOT}/usr/lib/libz.so) ${SHED_FAKEROOT}/usr/lib/libz.so
+make -j $SHED_NUM_JOBS
+make "DESTDIR=${SHED_FAKE_ROOT}" install
+mkdir -pv ${SHED_FAKE_ROOT}/lib
+mv -v ${SHED_FAKE_ROOT}/usr/lib/libz.so.* ${SHED_FAKE_ROOT}/lib
+ln -sfv ../../lib/$(readlink ${SHED_FAKE_ROOT}/usr/lib/libz.so) ${SHED_FAKE_ROOT}/usr/lib/libz.so
