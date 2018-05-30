d458f513237494c5aee76c3146d6bd3c6bb9252b
Auston Stewart <auston@shedbuilt.net>
2018-05-29 17:47:28 -1000

    Updating for blank

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-./configure --prefix=/usr
-make -j $SHED_NUM_JOBS
-make "DESTDIR=${SHED_FAKE_ROOT}" install
-mkdir -pv ${SHED_FAKE_ROOT}/lib
-mv -v ${SHED_FAKE_ROOT}/usr/lib/libz.so.* ${SHED_FAKE_ROOT}/lib
-ln -sfv ../../lib/$(readlink ${SHED_FAKE_ROOT}/usr/lib/libz.so) ${SHED_FAKE_ROOT}/usr/lib/libz.so
+./configure --prefix=/usr &&
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install &&
+mkdir -pv "${SHED_FAKE_ROOT}/lib" &&
+mv -v ${SHED_FAKE_ROOT}/usr/lib/libz.so.* "${SHED_FAKE_ROOT}/lib" &&
+ln -sfv ../../lib/$(readlink "${SHED_FAKE_ROOT}/usr/lib/libz.so") "${SHED_FAKE_ROOT}/usr/lib/libz.so"
